aa7cea75013a811b02919430c4b09bf8c844731e
W-Zama <wzama4667@gmail.com>
2023-11-04 04:50:03 +0900

    exmaple.cppとexample.shの追加

---
+++ example.cpp
@@ -0,0 +1,27 @@
+#include "compiler.hpp"
+
+using namespace std;
+
+int main()
+{
+    // インスタンス生成
+    Compiler compiler;
+
+    // プログラムの文字列をセットする
+    // compiler.set_program_string("A:=0.");
+
+    // ファイル名をセットする
+    string filename = "sample/sample";
+    compiler.set_program_file(filename + ".pas");
+
+    // コンパイル
+    compiler.compile();
+
+    // 結果の表示
+    compiler.print_result();
+
+    // ファイルに結果を出力
+    compiler.output_file(filename + ".obj");
+
+    return 0;
+}
