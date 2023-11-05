f1a731ce186d19fe010a3113728dd560ce157102
W-Zama <wzama4667@gmail.com>
2023-11-05 20:05:01 +0900

    ゲッタとセッタの追加2

--- command.hpp
+++ command.hpp
@@ -26,12 +26,18 @@ typedef enum {
 } Mnemonic;
 
 class Command {
-public:
+private:
     Mnemonic mnemonic;      // ニモニック (GETなど)
     optional<int> operand;  // オペランド (ニモニックの演算対象の値)
+
+public:
     static map<int, string> mnemonic2string_map;    // ニモニックの列挙型と文字列の対応表
 
     Command(Mnemonic mnemonic, optional<int> operand);
+    Mnemonic get_mnemonic(void);
+    void set_mnemonic(Mnemonic mnemonic);
+    optional<int> get_operand(void);
+    void set_operand(optional<int> operand);
 };
 
 #endif // _COMMAND_HPP_
